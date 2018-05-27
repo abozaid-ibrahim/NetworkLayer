a787916225846ccdd644ef885e78d1799b5a1cd2
abuzeid-ibrahim <abuzeid7@gmail.com>
2018-05-28 01:52:20 +0200

    default code

---
+++ NetworkLayer/default_structre/response/DataResponse+Decodable.swift
@@ -0,0 +1,28 @@
+//
+//  AlamofireResponse.swift
+//  NetworkLayer
+//
+//  Created by abuzeid ibrahim on 5/16/18.
+//  Copyright © 2018 abuzeid ibarhim. All rights reserved.
+//
+/**
+import Alamofire
+extension DataResponse {
+//    @discardableResult
+     func decodable<T:Decodable>( res: T.Type)throws ->T  {
+        do {
+            guard let data = self.data else {
+                throw APIError.dataIsNil
+            }
+            let todo = try JSONDecoder().decode(res.self, from: data)
+            return todo
+        } catch let err {
+            print("Parse Json Error\(err)")
+            throw APIError.jsonParsingFail
+        }
+    }
+    
+    
+  
+}
+*/
